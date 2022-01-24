83f8481198fcf0a47241a2fd958863154860e47b
melonMochiRoll <soulhwasin@gmail.com>
2022-01-24 20:51:31 +0900

    Update

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,28 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe() );
+  app.enableCors({ origin: true, credentials: true });
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  app.use(cookieParser() );
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+      } }));
 
   const port = process.env.PORT || 3005;
   await app.listen(port);
