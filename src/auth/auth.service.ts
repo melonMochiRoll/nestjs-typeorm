0208d818b1650bee26ef2a15d10572e844e214de
melonMochiRoll <soulhwasin@gmail.com>
2022-01-29 17:45:09 +0900

    Fix method name login -> signjwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/entities';
 import { Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
@@ -8,7 +9,8 @@ import bcrypt from 'bcrypt';
 export class AuthService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>) {}
+    private userRepository: Repository<User>,
+    private jwtService: JwtService ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userRepository.findOne({ email },
@@ -24,5 +26,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async signjwt(user: User) {
+    const payload = { sub: user.id, username: user.email, roles: user.role };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
