e61a806e8d11a0116565839b84aa483ac3fac44a
melonMochiRoll <soulhwasin@gmail.com>
2022-02-04 01:18:38 +0900

    Add strategies/jwt, local strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "src/common/interfaces";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    })
+  }
+
+  async validate(payload: JwtPayload) {
+    return { userId: payload.sub, email: payload.username, role: payload.role }
+  }
+}
