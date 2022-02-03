81d122382913de22f3638fbe8a99f420a7d7686d
melonMochiRoll <soulhwasin@gmail.com>
2022-02-04 01:20:41 +0900

    Update jwtstrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,19 +3,20 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { User } from 'src/entities';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy, JwtStrategy } from './strategies';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '15m' },
+      signOptions: { expiresIn: 900 },
     }),
     TypeOrmModule.forFeature([ User ]),
     PassportModule.register({ session: true }) ],
   providers: [
     AuthService,
+    JwtStrategy,
     LocalStrategy ],
   exports: [
     AuthService ]
