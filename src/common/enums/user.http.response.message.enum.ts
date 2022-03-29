12f36086f089853032f43b25a28fe0d7de2d589f
melonMochiRoll <soulhwasin@gmail.com>
2022-03-29 21:55:00 +0900

    Rename filename

--- src/common/enums/user.http.response.message.enum.ts
+++ src/common/enums/user.http.response.message.enum.ts
@@ -1,4 +1,5 @@
-export enum ApiHttpResponse {
+export enum UserHttpResponseMessage {
   EXIST_EMAIL = '이미 존재하는 이메일입니다.',
+  EXIST_NICKNAME = '이미 존재하는 닉네임입니다.',
   INVALID_LOGIN = '이메일 혹은 비밀번호를 확인 해 주세요',
 };
