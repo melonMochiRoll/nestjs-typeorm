c3b04e86a952d7b8ef82d1048244640a0109a9e2
melonMochiRoll <soulhwasin@gmail.com>
2022-03-30 23:41:30 +0900

    Add some enum

--- src/common/enums/user.http.response.message.enum.ts
+++ src/common/enums/user.http.response.message.enum.ts
@@ -1,5 +1,7 @@
 export enum UserHttpResponseMessage {
   EXIST_EMAIL = '이미 존재하는 이메일입니다.',
+  NOTEXIST_EMAIL = '존재하지 않는 이메일입니다.',
   EXIST_NICKNAME = '이미 존재하는 닉네임입니다.',
+  NOTEXIST_NICKNAME = '존재하지 않는 닉네임입니다.',
   INVALID_LOGIN = '이메일 혹은 비밀번호를 확인 해 주세요',
 };
