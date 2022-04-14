eb51a1cf3aa95a6bbd2fd9ac000785ff73654af4
melonMochiRoll <soulhwasin@gmail.com>
2022-04-15 00:11:49 +0900

    Setting redisClient

--- src/loaders/nestjs.ts
+++ src/loaders/nestjs.ts
@@ -4,8 +4,15 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import helmet from "helmet";
 import passport from "passport";
+import { createClient } from 'redis';
 
 export const nestjsLoader = async (app: NestExpressApplication) => {
+  let RedisStore = require('connect-redis')(session);
+  const redisClient = createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASSWORD,
+  });
+
   app.useGlobalPipes(new ValidationPipe() );
   app.enableCors({
     origin: true,
@@ -15,13 +22,14 @@ export const nestjsLoader = async (app: NestExpressApplication) => {
 
   app.use(cookieParser() );
   app.use(session({
-      secret: `${process.env.COOKIE_SECRET}`,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-      }
-    }));
+    secret: `${process.env.COOKIE_SECRET}`,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+    },
+    store: new RedisStore({ client: redisClient, logErrors: true }),
+  }));
   app.use(passport.initialize());
   app.use(passport.session());
 }
