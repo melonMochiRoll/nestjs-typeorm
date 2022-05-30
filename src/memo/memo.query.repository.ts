a0b9d304462765876378651b7f11500f7e3365d0
melonMochiRoll <soulhwasin@gmail.com>
2022-05-30 23:43:57 +0900

    FIx getMemosByFolderName

--- src/memo/memo.query.repository.ts
+++ src/memo/memo.query.repository.ts
@@ -1,15 +1,15 @@
 import { MemoCount } from "src/common/interfaces";
 import { Memo } from "src/entities";
-import { createQueryBuilder, EntityRepository, Repository } from "typeorm";
+import { createQueryBuilder, EntityRepository, getRepository, Repository } from "typeorm";
 
 @EntityRepository(Memo)
 export class MemoQueryRepository extends Repository<Memo> {
   async getMemoCount(
     userId: number,
     ): Promise<MemoCount[]> {
-    const folderCount = await createQueryBuilder()
+    const folderCount = await getRepository(Memo)
+      .createQueryBuilder('memo')
       .select('memo.folderName AS folderName')
-      .from(Memo, 'memo')
       .addSelect('COUNT(*) AS count')
       .where('memo.userId = :userId', { userId })
       .groupBy('memo.folderName')
@@ -22,9 +22,9 @@ export class MemoQueryRepository extends Repository<Memo> {
     userId: number,
     folderName: string,
   ): Promise<Memo[]> {
-    const memos = await createQueryBuilder()
-      .select('memo')
-      .from(Memo, 'memo')
+    const memos = await getRepository(Memo)
+      .createQueryBuilder('memo')
+      .leftJoinAndSelect('memo.tags', 'tag')
       .where('memo.userId = :userId', { userId })
       .andWhere('memo.folderName = :folderName', { folderName })
       .orderBy('memo.updatedAt', 'DESC')
